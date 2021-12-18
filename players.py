58d1652dca595f7ca7197ae81af6c40238069733
Zack <rubinzack40@gmail.com>
2021-12-18 04:52:50 -0500

    added drawing card methods for player class

--- players.py
+++ players.py
@@ -1,12 +1,27 @@
+from cards import Cards
+
 class Player:
     HAND_MAX=5
+    hand=[]
     def __init__(self, name: str):
         self.name = name
-        self.hand = []
 
     def team(self, team: str):
         self.team = team
 
-    # def draw_card(self):
-    #     # add card from offensive/defensive pile to hand
-    #     self.hand.append(card)
+    def draw_offensive_card(self, offensive_deck):
+        # add card from offensive/defensive pile to hand
+        if len(self.hand) < Player.HAND_MAX:
+            self.hand.append(offensive_deck.pop())
+
+    def draw_defensive_card(self, defensive_deck):
+        if len(self.hand) < Player.HAND_MAX:
+            self.hand.append(defensive_deck.pop())
+
+    def clear_hand(self):
+        self.hand.clear()
+
+
+
+# doesnt pop from live deck... 
+
